9d79737b0f05b41a2f22c14724616f3e884910a7
kiranrojjha <kiranrojjha@gmail.com>
2026-04-15 21:14:14 +0530

    Update notion_task.py

--- notion_task.py
+++ notion_task.py
@@ -48,6 +48,73 @@
 
 # some problems
 
+# 1
+# translator= {
+#        "kitab":"book",
+#        "kapi":"notebook",
+#        "chata":"umbrella"
+# }
+# x=input("enter the word to translate")
+# print(translator[x])
+
+# 2
+# s=set()
+# s1=int(input("enter the number"))
+# s.add(s1)
+# s2=int(input("enter the number"))
+# s.add(s2)
+# s3=int(input("enter the number"))
+# s.add(s3)
+# s4=int(input("enter the number"))
+# s.add(s4)
+# s5=int(input("enter the number"))
+# s.add(s5)
+# print(s)
+
+# 3
+# p={}
+# y=input("enter the name")
+# x=input("enter the language")
+# p.update({y:x})
+# print(p)
+
+# 4
+# age = int(input("enter your age ")) 
+# if age >= 18: 
+#   print("You are an adult.") 
+#   print("You can vote.") 
+# else: 
+#  print("You are a minor.") 
+#  print("You cannot vote.") 
+
+# 5
+# p1="Make a lot of money"
+# p2="buy now"
+# p3="subscribe this"
+# p4="click this"
+# message=input(" enter your message ")
+# if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+#  print("this is a spam message")
+
+# else:
+#  print("you are good to go")
+
+# 6
+# name=input("enter your name :")
+# length=len(name)
+# if(length>=10):
+#     print("name contains mre than 10 letters", length)
+# else:
+#     print("the name contains less than 10 characters")
+
+# 7
+# name='''kiran
+# payal
+# priyansha'''
+# print(name.find("kiran"))
+
+# 8
+# print("emoji: \N{BLACK HEART SUIT}")
 
 
 
